d3c27a303932dd848d6086bd456c795482256305
HARShaaYadav <160042665+HARShaaYadav@users.noreply.github.com>
2026-01-14 22:11:17 +0530

    Commiting CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 const App = () => {
-  return ( <div className="h-screen bg-black text-white">
- <form className="flex flex-col p-10">
-  <input type="text" placeholder="Enter Notes Heading " className="px-5 py-2 border-2 rounded" />
-  <input type="text" placeholder="Enter Details"/>
+  return ( <div className='h-screen bg-black text-white'>
+ <form className="flex items-start justify-between   p-10">
+  <div className="flex items-start w-1/2 gap-4 flex-col">
+  <input type="text" placeholder="Enter Notes Heading "
+   className="px-5 py-2 border-2 rounded w-full outline-none" />
+  <input type="text" placeholder="Enter Details"
+  className="px-5 py-2 border-2 rounded w-full outline-none"/>
+  <button className="bg-white text-black px-5 py-2 rounded w-1/2 ">Add Note</button>
+   </div>
+   
  </form>
   </div>
    )
